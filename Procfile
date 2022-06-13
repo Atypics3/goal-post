d2d05637c039a92aa04063102f16d218e992b048
Edwin <edwinhuang@outlook.com>
2022-06-13 13:43:29 -0700

    updated

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: npm run dev
-heroku ps: scale web=1
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT;
+const path = require("path");
 
 connectDB();
 
@@ -17,14 +18,17 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
+	// static assets
 	app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+	// loads frontend/build/index.html
 	app.get("*", (req, res) =>
 		res.sendFile(
 			path.resolve(__dirname, "../", "frontend", "build", "index.html")
 		)
 	);
 } else {
+	// if not set in production
 	app.get("/", (req, res) => res.send("Please set to production"));
 }
 
