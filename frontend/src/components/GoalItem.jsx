0af9d39180a6cd69a96f529386f73cfb8f688ee2
Edwin <edwinhuang@outlook.com>
2022-06-17 22:18:45 -0700

    added ability to edit goals

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,16 +1,54 @@
 import { useDispatch } from "react-redux";
-import { deleteGoal } from "../features/goals/goalSlice";
+import { useState } from "react";
+import { deleteGoal, updateGoal } from "../features/goals/goalSlice";
 
 function GoalItem({ goal }) {
+	const [text, setText] = useState("");
+
 	const dispatch = useDispatch();
+
+	const onEdit = (e) => {
+		e.preventDefault();
+		setText(text);
+	};
+
+	const onUpdate = {
+		id: goal._id,
+		text: text,
+	};
 
 	return (
 		<div className="goal">
 			<div>{new Date(goal.createdAt).toLocaleString("en-US")}</div>
 			<h2>{goal.text}</h2>
+			{/* delete goal */}
 			<button onClick={() => dispatch(deleteGoal(goal._id))} className="close">
 				X
 			</button>
+
+			{/* update goal */}
+			<form onSubmit={onEdit}>
+				<div className="form-group">
+					<input
+						type="text"
+						name="text"
+						id="text"
+						value={text}
+						onChange={(e) => setText(e.target.value)}
+					/>
+				</div>
+
+				<div className="form-group">
+					<button
+						className="btn btn-avg"
+						onClick={() => {
+							dispatch(updateGoal(onUpdate));
+						}}
+					>
+						Update
+					</button>
+				</div>
+			</form>
 		</div>
 	);
 }
